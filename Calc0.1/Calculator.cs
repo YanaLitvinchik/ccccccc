db423614df9cf699d89b7e4f4b677d5d21ac3c5c
YanaLitvinchik <litvinchikyana@ukr.net>
2018-10-13 14:10:12 +0300

    Add project files.

---
+++ Calc0.1/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc0._1
+{
+    class Calculator : INotifyPropertyChanged
+    {
+        public string current { get; set; }
+        public Calculator()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
